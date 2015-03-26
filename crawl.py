30dc7c04b0f811aa79ac1bf51313f1a59453aaa1
Andy Chosak <chosak@gmail.com>
2015-03-25 21:59:59 -0400

    working crawler

--- crawl.py
+++ crawl.py
@@ -1,9 +1,14 @@
+import csv
+import itertools
 import logging
 import requests
+import socket
 import urllib
 
 from BeautifulSoup import BeautifulSoup
+from hashlib import md5
 from itertools import izip
+from memcache import Client
 
 
 logging.basicConfig(level=logging.INFO)
@@ -11,24 +16,88 @@ logger = logging.getLogger(__name__)
 
 
 class Crawler(object):
+    def __init__(self):
+        self.cache = self.get_cache()
+        self.step = 25
+
+
+    def get_cache(self):
+        try:
+            socket.create_connection(('localhost', 11211))
+            logger.info('using local memcached')
+
+            return Client(['localhost:11211'])
+        except socket.error:
+            logger.info('no local memcached')
+            return None
+
+
     def crawl(self):
         year = 2014
-        gender = 1
-        offset = 0
+        genders = (1, 2)
+        results = []
         
-        html = self.query(year, gender, offset)
-        results = self.parse(html)
+        for gender in genders:
+            gender_results = list(
+                itertools.chain(*self.results_generator(year, gender))
+            )
+
+            results.extend(gender_results)
+
+        return results
 
 
-    def query(self, year, gender, offset):
-        url = self.url(year, gender)
+    def results_generator(self, year, gender):
+        offset = 0
 
+        while True:
+            params = {
+                'year': year,
+                'gender': gender,
+                'offset': offset,
+            }
+
+            html = self.query(params)
+            results = self.parse(html)
+
+            if not results:
+                return
+
+            yield results
+
+            offset += self.step
+
+
+    def query(self, params):
+        logger.info('querying for {}'.format(params))
+
+        url = self.url(params['year'], params['gender'])
+        data={
+            'start': params['offset'] + 1,
+        }
+
+        cache_key = md5(url + str(data)).hexdigest()
+
+        if self.cache:
+            html = self.cache.get(cache_key)
+
+            if html is not None:
+                logger.info('retrieved from cache')
+                return html
+
+        logger.info('querying server')
+        html = self.query_server(url, data)
+
+        if self.cache:
+            self.cache.set(cache_key, html, time=0)
+
+        return html
+
+
+    def query_server(self, url, data):
         response = requests.post(
             url,
-            data={
-                'start': offset + 1,
-                'next': 'Next 25 Records',
-            },
+            data=data,
             headers={
                 'Origin': 'http://registration.baa.org',
                 'Content-Type': 'application/x-www-form-urlencoded',
@@ -65,7 +134,7 @@ class Crawler(object):
             'VarTargetCount': 1000,
             'headerexists': 'Yes',
             'mode': 'results',
-            'records': 25,
+            'records': self.step,
             'queryname': 'SearchResults',
         }
 
@@ -76,7 +145,7 @@ class Crawler(object):
         soup = BeautifulSoup(html)
 
         tbody = soup.find('tbody')
-        trs = tbody.findAll('tr')
+        trs = tbody.findAll('tr', recursive=False)
 
         def pairwise(iterable):
             i = iter(iterable)
@@ -89,9 +158,50 @@ class Crawler(object):
 
 
     def parse_athlete(self, summary, splits):
-        pass
+        results = {}
+
+        summary_tds = summary.findAll('td')
+        summary_keys = [
+            'bib', 'name', 'age', 'gender', 'city', 'state', 'country',
+            'citizenship'
+        ]
+
+        results.update({
+            summary_keys[i]: summary_tds[i].text.replace('&nbsp;', '') \
+            for i in range(len(summary_keys))
+        })
+
+        splits_trs = splits.findAll('tr')
+        splits_tds = list(itertools.chain(*[
+            tr.findAll('td') for tr in (splits_trs[1], splits_trs[3])
+        ]))
+        splits_keys = [
+            '5k', '10k', '15k', '20k', 'Half', '25k', '30k', '35k', '40k',
+            'pace', 'projected time', 'official time', 'place_overall',
+            'place_gender', 'place_division'
+        ]
+
+        results.update({
+            splits_keys[i]: splits_tds[i].text.replace('&nbsp;', '') \
+            for i in range(len(splits_keys))
+        })
+
+        return results
     
 
 
 if '__main__' == __name__:
-    Crawler().crawl()
+    results = Crawler().crawl()
+
+    filename = 'crawl.csv'
+    logger.info('writing {} results to {}'.format(
+        len(results),
+        filename
+    ))
+
+    with open(filename, 'wb') as f:
+        writer = csv.DictWriter(f, results[0].keys())
+        writer.writeheader()
+
+        for result in results:
+            writer.writerow(result)
